09c71b0d23cfd00cf42c8fe9625c357e9e912947
CristhianWS <cristhiansilvaalves@gmail.com>
2022-10-25 09:11:28 -0300

    adicionado rota dos components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cadastro from './pages/Cadastro/Cadastro'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cadastro from "./pages/Cadastro/Cadastro";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <main className='container'>
-
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home /> }></Route>
-      <Route path='/cadastro' element={<Cadastro /> }></Route>
-    </Routes>
-    </BrowserRouter>
+    <main className="container">
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cadastro" element={<Cadastro />}></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
