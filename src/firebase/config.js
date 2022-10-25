2901d6a0cf89238bae4702bef5be7e5185856b7e
CristhianWS <cristhiansilvaalves@gmail.com>
2022-10-25 10:21:38 -0300

    Adicionado firebase e firebase autenticação

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvab2vqYBBSGKnMnWP55UrFDrXgRdvJZU",
+  authDomain: "labwebdsmautenticar.firebaseapp.com",
+  projectId: "labwebdsmautenticar",
+  storageBucket: "labwebdsmautenticar.appspot.com",
+  messagingSenderId: "342209996209",
+  appId: "1:342209996209:web:504f816d034002a913c990"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
