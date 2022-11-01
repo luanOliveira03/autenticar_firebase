0a65d023f357b3a5e9f2b1a1c712edc514ad6ae2
Cristhian <cristhiansilvaalves@gmail.com>
2022-11-01 08:16:32 -0300

    configuração firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,12 @@ const firebaseConfig = {
   projectId: "labwebdsmautenticar",
   storageBucket: "labwebdsmautenticar.appspot.com",
   messagingSenderId: "342209996209",
-  appId: "1:342209996209:web:504f816d034002a913c990"
+  appId: "1:342209996209:web:504f816d034002a913c990",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const authentication = getAuth(app);
+
+export { authentication };
